a3095a1721d9730a6a0e6e80509f25e31821c738
Derek Morales <derekmorales@newhuaweiapstudent-10-59-111-39.ncl.ac.uk>
2019-10-31 17:20:54 +0000

    map

--- BarFly Reformat/FirstViewController.swift
+++ BarFly Reformat/FirstViewController.swift
@@ -6,13 +6,52 @@
 //  Copyright © 2019 LoFi Games. All rights reserved.
 //
 
+import MapKit
 import UIKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    @IBOutlet var myNavBar: UINavigationBar!
+    @IBOutlet var myMapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        myMapView.delegate = self
+        myMapView.mapType = .standard
+        myMapView.isZoomEnabled = true
+        myMapView.isScrollEnabled = true
+        
+        if let coor = myMapView.userLocation.location?.coordinate{
+            myMapView.setCenter(coor, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        myMapView.mapType = MKMapType.standard
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        myMapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "Derek Morales"
+        annotation.subtitle = "current location"
+        myMapView.addAnnotation(annotation)
+        
+        //centerMap(locValue)
     }
 
 
