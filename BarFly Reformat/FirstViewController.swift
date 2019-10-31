8ebe374ff001b961cbfcde8114db2483a073a595
Derek Morales <derekmorales@newhuaweiapstudent-10-59-111-39.ncl.ac.uk>
2019-10-31 14:14:29 +0000

    Start

---
+++ BarFly Reformat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BarFly Reformat
+//
+//  Created by Derek Morales on 10/31/19.
+//  Copyright © 2019 LoFi Games. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BarFly Reformat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BarFly Reformat
+//
+//  Created by Derek Morales on 10/31/19.
+//  Copyright © 2019 LoFi Games. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
